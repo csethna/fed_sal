f865fe1b22a39a5eafcfea9e33e7e8649e63c8cb
Cyrus Sethna <csethna@gmail.com>
2017-05-30 15:34:00 -0500

    Grade lookup
    Working to integrate lookup functionality based on employee location,
    grade, and step when name lookup is unavailable.

---
+++ csethna_example.py
@@ -0,0 +1,135 @@
+import requests, re, json, sys
+from time import sleep
+
+
+html_filter = re.compile('<[^<]+?>')
+LOOKUP_URL = 'https://www.federalpay.org/employees/lookup'
+WORK_SECONDS_IN_YEAR = 60 * 60 * 40 * 52
+
+
+def input_name():
+        first_name = raw_input("first name:")
+        if not first_name:
+            return None, None
+        last_name = raw_input("last name:")
+        if not last_name:
+            return None, None
+
+        return first_name, last_name
+
+
+
+def fetch_pay_for_name(first_name, last_name):
+    # Note how the params are a dictionary of key-value pairs;
+    # requests will build the query params into the URL before making the request
+    # Think about how you could build the params programmatically to request info on any given nameprint "first name:"
+    r = requests.get(LOOKUP_URL, params={'year': '2015', 'firstName': first_name, 'lastName': last_name, 'state': 'IL'})
+    # The two lines below both do two things at once; feel free to make these separate lines in your code
+    # if that makes it easier to follow.
+    response = json.loads(html_filter.sub('', r.content))
+    # print response
+    try:
+        pay = int(response['result'][0]['pay'])
+    except ValueError:
+        print ' '.join(["Salary redacted for", first_name, last_name])
+        pay = 0
+    except IndexError:
+        print ' '.join(["Employee", first_name, last_name, "not found, please check name."])
+        pay = 0
+    return pay
+
+
+########################## NEW STUFF ###############################
+
+total_pay = []
+# Get pay for all attendees
+while True:
+    try:
+        first, last = input_name()
+        if not first or not last:
+            break
+        total_pay.append(fetch_pay_for_name(first, last))
+        print total_pay
+    except KeyboardInterrupt:
+        break
+
+cost_per_second = float(sum(total_pay)) / WORK_SECONDS_IN_YEAR
+meeting_cost = 0.00
+print
+# system interrupt
+while True:
+    try:
+        sys.stdout.write("\rCurrent meeting is costing taxpayers: ${0:.2f}".format(meeting_cost))
+        sys.stdout.flush()
+        sleep(1)
+        meeting_cost += cost_per_second
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+
+
+
+# filtered_result = """{
+#     "queryExecutionTime": 0.0003,
+#     "result": [
+#         {
+#             "name": "Robert W. Steetz",
+#             "slug": "steetz-robert-w",
+#             "agency_slug": "federal-acquisition-service",
+#             "occupation_slug": "miscellaneous-administration-and-program",
+#             "year": "2015",
+#             "occupation": "Miscellaneous Administration and Program",
+#             "agency": "Federal Acquisition Service",
+#             "state": "ILLINOIS",
+#             "country": "United States",
+#             "pay": "109759",
+#             "location": "Illinois",
+#             "formatted_pay": "$109,759"
+#         }
+#     ]
+# }"""
+#
+#
+# raw_json_result = """{
+#     "queryExecutionTime": 0.0003,
+#     "result": [
+#         {
+#             "name": "<a href=/employees/federal-acquisition-service/steetz-robert-w>Robert W. Steetz</a>",
+#             "slug": "steetz-robert-w",
+#             "agency_slug": "federal-acquisition-service",
+#             "occupation_slug": "miscellaneous-administration-and-program",
+#             "year": "2015",
+#             "occupation": "<a href=/employees/occupations/miscellaneous-administration-and-program>Miscellaneous Administration and Program</a>",
+#             "agency": "<a href=/employees/federal-acquisition-service>Federal Acquisition Service</a>",
+#             "state": "ILLINOIS",
+#             "country": "United States",
+#             "pay": "109759",
+#             "location": "Illinois",
+#             "formatted_pay": "$109,759"
+#         }
+#     ]
+# }"""
+#
+#
+# # Here is the URL to fetch salary data for a given GS and step.
+# gs_lookup_url = 'https://www.federalpay.org/api/calculator/GS?s=2&state=Illinois&county=Cook&grade=10&step=4&year=2017'
+#
+# sample_output = """
+#     {
+#         "Rank": "Captain",
+#         "BaseSalary": "52394",
+#         "AdjustedSalary": "66461.79",
+#         "IsSalaryCapped": "False",
+#         "WeeksToIncreaseStep": "104",
+#         "LocalityAreaName": "Chicago",
+#         "LocalityAreaLongName": "CHICAGO-NAPERVILLE, IL-IN-WI",
+#         "LocalityRate": "0.2685",
+#         "Education": "Master's Degree",
+#         "Experience": "At least 1 year experience at GS-9 or equivalent",
+#         "s": "2",
+#         "year": "2017",
+#         "grade": "10",
+#         "step": "4",
+#         "state": "Illinois",
+#         "county": "Cook"
+#     }"""
