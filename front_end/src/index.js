233df37bd672ea0986be16d85ea8eb5cfdb11dba
Cyrus Sethna <csethna@gmail.com>
2017-07-03 15:16:39 -0500

    front-end
    working on learning the tools for the front end

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => alert('whoo whoo')}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -13,8 +13,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+      );
   }
 
   render() {
@@ -57,6 +75,7 @@ class Game extends React.Component {
       </div>
     );
   }
+
 }
 
 // ========================================
